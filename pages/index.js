872cdb2089fe2cd9c94e00cf7da50a1aeca63377
raselcsedev <raselcse.dev@gmail.com>
2023-11-11 16:17:09 +0600

    home page avatar design

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Home = () => {
             exit="hidden"
             className="h1"
           >
-            Front-End Web <span className="text-accent">Developer</span>
+            Front-End Web <br /> <span className="text-accent">Developer</span>
           </motion.h1>
           <motion.p
             variants={fadeIn("down", 0.3)}
@@ -45,7 +45,20 @@ const Home = () => {
           </motion.div>
         </div>
       </div>
-      <div></div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0 ">
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0 ">
+        </div>
+        <ParticlesContainer />
+        <motion.div
+        variants={fadeIn("up", 0.5)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        transition={{duration:1, ease: 'easeInOut'}}
+        className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%] ">
+          <Avatar />
+        </motion.div>
+      </div>
     </div>
   );
 };
